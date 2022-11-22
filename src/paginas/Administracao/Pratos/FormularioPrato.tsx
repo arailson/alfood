f4dc679d5bfc7fd381d03cf9877ea5ac9345a61f
Arailson <arailson.vieira@gmail.com>
2022-11-22 18:19:53 -0300

    Formulario de pratos

---
+++ src/paginas/Administracao/Pratos/FormularioPrato.tsx
@@ -0,0 +1,124 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import Restaurante from "../../../componentes/ListaRestaurantes/Restaurante";
+import http from "../../../http";
+import IRestaurante from "../../../interfaces/IRestaurante";
+import ITag from "../../../interfaces/ITag";
+
+const FormularioPrato = () => {
+  const [nomePrato, setNomePrato] = useState("");
+  const [descricao, setDescricao] = useState("");
+
+  const [imagem, setImagem] = useState<File | null>(null);
+
+  const [tag, setTag] = useState("");
+  const [tags, setTags] = useState<ITag[]>([]);
+
+  const [restaurante, setRestaurante] = useState("");
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+
+  const selecionarArquivo = (evento: React.ChangeEvent<HTMLInputElement>) => {
+    if (evento.target.files?.length) {
+      setImagem(evento.target.files[0]);
+    } else {
+      setImagem(null);
+    }
+  };
+
+  useEffect(() => {
+    http
+      .get<{ tags: ITag[] }>("tags/")
+      .then((resposta) => setTags(resposta.data.tags));
+    http
+      .get<IRestaurante[]>("restaurantes/")
+      .then((resposta) => setRestaurantes(resposta.data));
+  }, []);
+
+  const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+  };
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          flexGrow: 1,
+        }}
+      >
+        <Typography component="h1" variant="h6">
+          Formulário de Pratos
+        </Typography>
+        <Box component="form" sx={{ width: "100%" }} onSubmit={aoSubmeterForm}>
+          <TextField
+            value={nomePrato}
+            onChange={(evento) => setNomePrato(evento.target.value)}
+            label="Nome do Prato"
+            variant="standard"
+            fullWidth
+            required
+            margin="dense"
+          />
+          <TextField
+            value={descricao}
+            onChange={(evento) => setDescricao(evento.target.value)}
+            label="Descrição"
+            variant="standard"
+            fullWidth
+            required
+            margin="dense"
+          />
+          <FormControl margin="dense" fullWidth>
+            <InputLabel id="select-tag">Tag</InputLabel>
+            <Select
+              labelId="select-tag"
+              value={tag}
+              onChange={(evento) => setTag(evento.target.value)}
+            >
+              {tags.map((tag) => (
+                <MenuItem key={tag.id} value={tag.id}>
+                  {tag.value}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl margin="dense" fullWidth>
+            <InputLabel id="select-Restaurante">Restaurante</InputLabel>
+            <Select
+              labelId="select-Restaurante"
+              value={restaurante}
+              onChange={(evento) => setRestaurante(evento.target.value)}
+            >
+              {restaurantes.map((restaurante) => (
+                <MenuItem key={restaurante.id} value={restaurante.id}>
+                  {restaurante.nome}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <input type="file" onChange={selecionarArquivo} />
+          <Button
+            sx={{ marginTop: 1 }}
+            type="submit"
+            variant="outlined"
+            fullWidth
+          >
+            Salvar
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default FormularioPrato;
