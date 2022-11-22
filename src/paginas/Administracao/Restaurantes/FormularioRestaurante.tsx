645215760c58ce7c326a28ce88d48dafcc7d5172
Arailson <arailson.vieira@gmail.com>
2022-11-22 12:23:55 -0300

    Ajustando Layout do formulario de adicionar restaurante

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -24,12 +24,12 @@ const FormularioRestaurante = () => {
 
     if (parametros.id) {
       axios
-      .put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
-        nome: nomeRestaurante,
-      })
-      .then(() => {
-        alert("Restaurante atualizado com sucesso");
-      });
+        .put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
+          nome: nomeRestaurante,
+        })
+        .then(() => {
+          alert("Restaurante atualizado com sucesso");
+        });
     } else {
       axios
         .post("http://localhost:8000/api/v2/restaurantes/", {
@@ -41,17 +41,31 @@ const FormularioRestaurante = () => {
     }
   };
   return (
-    <form onSubmit={aoSubmeterForm}>
-      <TextField
-        value={nomeRestaurante}
-        onChange={(evento) => setNomeRestaurante(evento.target.value)}
-        label="Nome do Restaurante"
-        variant="standard"
-      />
-      <Button type="submit" variant="outlined">
-        Salvar
-      </Button>
-    </form>
+    <Box
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <Typography component="h1" variant="h6">
+        Formulário de Restaurantes
+      </Typography>
+      <Box component="form" onSubmit={aoSubmeterForm}>
+        <TextField
+          value={nomeRestaurante}
+          onChange={(evento) => setNomeRestaurante(evento.target.value)}
+          label="Nome do Restaurante"
+          variant="standard"
+          fullWidth
+          required
+        />
+        <Button
+          sx={{ marginTop: 1 }}
+          type="submit"
+          variant="outlined"
+          fullWidth
+        >
+          Salvar
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
